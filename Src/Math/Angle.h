35e82c144c664647253e5ed7bdae40ab39475ca5
AirGuanZ <AirGuanZ@gmail.com>
2018-09-29 16:22:45 +0800

    重写Angle组件，允许Deg（角度）和Rad（弧度）类间的隐式转换

--- Src/Math/Angle.h
+++ Src/Math/Angle.h
@@ -4,51 +4,55 @@
 
 AGZ_NS_BEG(AGZ::Math)
 
-template<typename T, int I>
-struct AngleTemplate
+template<typename T>
+struct Deg;
+
+template<typename T>
+struct Rad
 {
     T value;
-
-    constexpr AngleTemplate() : value(T(0.0)) { }
-    explicit constexpr AngleTemplate(T v) : value(v) { }
-    explicit constexpr AngleTemplate(Uninitialized_t) { }
+    
+    constexpr Rad() : value(T(0)) { }
+    explicit constexpr Rad(T v) : value(v) { }
+    explicit constexpr Rad(Uninitialized_t) { }
+    constexpr Rad(const Deg<T> &d);
 };
 
 template<typename T>
-using Rad = AngleTemplate<T, 0>;
+struct Deg
+{
+    T value;
+
+    constexpr Deg() : value(T(0)) { }
+    explicit constexpr Deg(T v) : value(v) { }
+    explicit constexpr Deg(Uninitialized_t) { }
+    constexpr Deg(Rad<T> r)
+        : value(T(180) / T(3.141592653589793238462643383) * r.value) { }
+};
 
 template<typename T>
-using Deg = AngleTemplate<T, 1>;
-
-template<typename T, int I>
-auto operator+(AngleTemplate<T, I> lhs, AngleTemplate<T, I> rhs)
+constexpr Rad<T>::Rad(const Deg<T> &d)
+    : value(T(3.141592653589793238462643383) / T(180) * d.value)
 {
-    return AngleTemplate<T, I>{ lhs.value + rhs.value };
+    
 }
 
-template<typename T, int I>
-auto operator-(AngleTemplate<T, I> lhs, AngleTemplate<T, I> rhs)
-{
-    return AngleTemplate<T, I>{ lhs.value - rhs.value };
-}
+#define ANGLE_OPERATORS(Type) \
+    template<typename T> constexpr auto operator+(Type<T> lhs, Type<T> rhs) \
+        { return Type<T>(lhs.value + rhs.value); } \
+    template<typename T> constexpr auto operator-(Type<T> lhs, Type<T> rhs) \
+        { return Type<T>(lhs.value - rhs.value); } \
+    template<typename T> constexpr auto operator-(Type<T> angle) \
+        { return Type<T>(-angle.value); } \
+    template<typename T> constexpr auto operator*(T lhs, Type<T> rhs) \
+        { return Type<T>(lhs * rhs.value); } \
+    template<typename T> constexpr auto operator*(Type<T> lhs, T rhs) \
+        { return Type<T>(lhs.value * rhs); } \
+    template<typename T> constexpr auto operator/(Type<T> lhs, T rhs) \
+        { return Type<T>(lhs.value / rhs); }
 
-template<typename T, int I>
-auto operator*(AngleTemplate<T, I> lhs, T rhs)
-{
-    return AngleTemplate<T, I>{ lhs.value * rhs };
-}
-
-template<typename T, int I>
-auto operator*(T lhs, AngleTemplate<T, I> rhs)
-{
-    return rhs * lhs;
-}
-
-template<typename T, int I>
-auto operator/(AngleTemplate<T, I> lhs, T rhs)
-{
-    return AngleTemplate<T, I>{ lhs.value / rhs };
-}
+ANGLE_OPERATORS(Rad)
+ANGLE_OPERATORS(Deg)
 
 using Degf = Deg<float>;
 using Degd = Deg<double>;
