d9c651ea62c2c5debc38ab75ef0a9275e6657ca7
AirGuanZ <airguanz@gmail.com>
2018-11-27 16:20:23 +0800

    RotateSH_L2

--- Src/Math/SH.h
+++ Src/Math/SH.h
@@ -3,6 +3,7 @@
 #include "../Misc/Common.h"
 #include "Mat3.h"
 #include "Tri.h"
+#include "Vec.h"
 #include "Vec3.h"
 
 namespace AGZ::Math {
@@ -13,7 +14,7 @@ namespace AGZ::Math {
 
 namespace SHImpl
 {
-    // See https://en.wikipedia.org/wiki/Table_of_spherical_harmonics
+    // 见https://en.wikipedia.org/wiki/Table_of_spherical_harmonics
     template<typename T, int L, int M> struct SHAux { };
 
     template<typename T>
@@ -111,6 +112,7 @@ namespace SHImpl
         }
     };
 
+    // 投影函数P，见https://airguanz.github.io/2018/11/20/SH-PRT.html
     template<typename T, int L> struct PAux { };
 
     template<typename T> struct PAux<T, 1>
@@ -123,6 +125,21 @@ namespace SHImpl
                 C * dir.y / r,
                 C * dir.z / r,
                 C * dir.x / r
+            );
+        }
+    };
+
+    template<typename T> struct PAux<T, 2>
+    {
+        static Vec<5, T> Eval(const Vec3<T> &dir)
+        {
+            auto ndir = dir.Normalize();
+            return Vec<5, T>(
+                SHAux<T, 2, -2>::Eval(ndir),
+                SHAux<T, 2, -1>::Eval(ndir),
+                SHAux<T, 2, 0>::Eval(ndir),
+                SHAux<T, 2, 1>::Eval(ndir),
+                SHAux<T, 2, 2>::Eval(ndir)
             );
         }
     };
@@ -180,7 +197,6 @@ auto GetSHByLM(int L, int M)
 template<typename T>
 void RotateSH_L0([[maybe_unused]] const Mat3<T> &M, [[maybe_unused]] T *SHCoef)
 {
-    AGZ_ASSERT(SHCoef);
     // l=0对应的SH是常量函数，其系数不受旋转的影响
 }
 
@@ -195,7 +211,6 @@ void RotateSH_L0([[maybe_unused]] const Mat3<T> &M, [[maybe_unused]] T *SHCoef)
 template<typename T>
 void RotateSH_L1(const Mat3<T> &M, T *SHCoef)
 {
-    static const T C     = Sqrt(3 / (4 * PI<T>));
     static const T INV_C = Sqrt(4 * PI<T> / 3);
 
     static const Mat3<T> INV_A(T(0),  T(0),  INV_C,
@@ -207,11 +222,65 @@ void RotateSH_L1(const Mat3<T> &M, T *SHCoef)
     auto PMN0 = SHImpl::PAux<T, 1>::Eval(M.GetCol(0));
     auto PMN1 = SHImpl::PAux<T, 1>::Eval(M.GetCol(1));
     auto PMN2 = SHImpl::PAux<T, 1>::Eval(M.GetCol(2));
-
     auto S = Mat3<T>::FromCols(PMN0, PMN1, PMN2);
-    Vec3<T> x(SHCoef[0], SHCoef[1], SHCoef[2]);
 
-    return S * (INV_A * x);
+    Vec3<T> x(SHCoef[0], SHCoef[1], SHCoef[2]);
+    x = S * (INV_A * x);
+
+    SHCoef[0] = x[0];
+    SHCoef[1] = x[1];
+    SHCoef[2] = x[2];
+}
+
+/**
+ * @brief 旋转l=2对应的SH函数系数
+ * 
+ * 推导见 https://airguanz.github.io/2018/11/20/SH-PRT.html
+ * 
+ * @param M 旋转矩阵
+ * @param SHCoef 待修改的系数，应包含5个元素
+ */
+template<typename T>
+void RotateSH_L2(const Mat3<T> &M, T *SHCoef)
+{
+    constexpr T K = T(0.7071067811865475);
+    static const Vec3<T> N2(K, K, T(0));
+    static const Vec3<T> N3(K, T(0), K);
+    static const Vec3<T> N4(T(0), K, K);
+
+    constexpr T K0 = T(0.91529123286551084);
+    constexpr T K1 = T(2 * 0.91529123286551084);
+    constexpr T K2 = T(1.5853309190550713);
+
+    // 计算A^{-1}x
+
+    Vec<5, T> InvAx(K0 * (-SHCoef[1] + SHCoef[3]) + K1 * SHCoef[4],
+                    K0 * (SHCoef[0] + SHCoef[3] + SHCoef[4]) + K2 * SHCoef[2],
+                    K1 * SHCoef[0],
+                    -K1 * SHCoef[3],
+                    -K1 * SHCoef[1]);
+
+    // 构造S
+    // IMPROVE：N是确定的，故P(M * N)可以优化
+
+    auto PMN0 = SHImpl::PAux<T, 2>::Eval(M.GetCol(0));
+    auto PMN1 = SHImpl::PAux<T, 2>::Eval(M.GetCol(2));
+    auto PMN2 = SHImpl::PAux<T, 2>::Eval(M * N2);
+    auto PMN3 = SHImpl::PAux<T, 2>::Eval(M * N3);
+    auto PMN4 = SHImpl::PAux<T, 2>::Eval(M * N4);
+
+    // 计算S(A^{-1}x)
+
+    SHCoef[0] = PMN0[0] * InvAx[0] + PMN1[0] * InvAx[1] + PMN2[0] * InvAx[2]
+              + PMN3[0] * InvAx[3] + PMN4[0] * InvAx[4];
+    SHCoef[1] = PMN0[1] * InvAx[0] + PMN1[1] * InvAx[1] + PMN2[1] * InvAx[2]
+              + PMN3[1] * InvAx[3] + PMN4[1] * InvAx[4];
+    SHCoef[2] = PMN0[2] * InvAx[0] + PMN1[2] * InvAx[1] + PMN2[2] * InvAx[2]
+              + PMN3[2] * InvAx[3] + PMN4[2] * InvAx[4];
+    SHCoef[3] = PMN0[3] * InvAx[0] + PMN1[3] * InvAx[1] + PMN2[3] * InvAx[2]
+              + PMN3[3] * InvAx[3] + PMN4[3] * InvAx[4];
+    SHCoef[4] = PMN0[4] * InvAx[0] + PMN1[4] * InvAx[1] + PMN2[4] * InvAx[2]
+              + PMN3[4] * InvAx[3] + PMN4[4] * InvAx[4];
 }
 
 } // namespace AGZ::Math
