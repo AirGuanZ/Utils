9863738a0be66f0431e1f210ba6ba255fb3b788f
Z Guan <airguanz@gmail.com>
2019-03-21 13:06:15 +0800

    auto-named scope guard

--- Src/AGZUtils/Misc/ScopeGuard.h
+++ Src/AGZUtils/Misc/ScopeGuard.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <functional>
 
@@ -33,3 +33,12 @@ public:
 };
 
 } // namespace AGZ
+
+/**
+ * @brief 定义一个自动命名的scope guard，离开定义作用域时自动调用指定的操作
+ * 
+ * 由于命名是根据行号自动进行的，很难对其调用dismiss操作。如有此需要，可显式使用 AGZ::ScopeGuard
+ */
+#define AGZ_SCOPE_GUARD(X)  AGZ_SCOPE_GUARD_IMPL0(X, __LINE__)
+#define AGZ_SCOPE_GUARD_IMPL0(X, LINE)  AGZ_SCOPE_GUARD_IMPL1(X, LINE)
+#define AGZ_SCOPE_GUARD_IMPL1(X, LINE) ::AGZ::ScopeGuard _autoScopeGuard##LINE([&] X)
