e79ea7d661c8e1d86e651a3c7d36f859503d52d6
Z Guan <airguanz@gmail.com>
2019-04-05 13:28:26 +0800

    Update HierarchyException.h

--- Src/AGZUtils/Exception/HierarchyException.h
+++ Src/AGZUtils/Exception/HierarchyException.h
@@ -6,73 +6,23 @@
 namespace AGZ
 {
     
-/**
- * @brief 层次式异常，通过AGZ_HIERARCHY_TRY和AGZ_HIERARCHY_WRAP捕获和再抛出，可以记录抛出路径上所有由HierarchyException记录的消息
- */
-class HierarchyException : public std::exception
+template<typename TOutputIterator>
+void ExtractHierarchyExceptions(const std::exception &e, TOutputIterator outputIterator)
 {
-    std::exception_ptr ptr_;
-    std::string what_;
-
-public:
-
-    /**
-     * @beief 一般来说由AGZ_HIERARCHY_WRAP调用，用户自己调用时 ptr 参数留空
-     */
-    explicit HierarchyException(std::string what, const std::exception_ptr &ptr = std::exception_ptr())
-        : ptr_(ptr), what_(std::move(what))
+    *outputIterator++ = e.what();
+    try
     {
-        
+        std::rethrow_if_nested(e);
     }
-
-    /**
-     * @brief 取得错误消息
-     */
-    char const *what() const noexcept override
+    catch(const std::exception &e2)
     {
-        return what_.c_str();
+        ExtractHierarchyExceptions(e2, outputIterator);
     }
-
-    /**
-     * @brief 由外向内，取得该Exception抛出路径上的所有由HierarchyException记录的错误消息
-     */
-    template<typename OutputIterator>
-    void GetAllMessages(OutputIterator outputIterator) const
+    catch(...)
     {
-        *outputIterator++ = what_;
-
-        try
-        {
-            throw ptr_;
-        }
-        catch(...)
-        {
-            auto p = std::current_exception();
-            while(p)
-            {
-                try
-                {
-                    std::rethrow_exception(p);
-                }
-                catch(const HierarchyException &e)
-                {
-                    *outputIterator++ = e.what_;
-                    p = e.ptr_;
-                }
-                catch(const std::exception &e)
-                {
-                    *outputIterator++ = std::string(e.what());
-                    p = std::exception_ptr();
-                }
-                catch(...)
-                {
-                    *outputIterator++ = std::string("an unknown exception was thrown");
-                    p = std::exception_ptr();
-                }
-            }
-        }
+        *outputIterator++ = std::string("an unknown exception was thrown");
     }
-};
+}
 
 #define AGZ_HIERARCHY_TRY try {
 
@@ -80,8 +30,7 @@ public:
     } \
     catch(...) \
     { \
-        auto ptr = std::current_exception(); \
-        throw ::AGZ::HierarchyException((MSG), ptr); \
+        std::throw_with_nested(std::runtime_error(MSG)); \
     }
 
 } // namespace AGZ
